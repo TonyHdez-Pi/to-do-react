1eb7382aa4ab1c55f337dff835080c2771027252
TonyHdez-Pi <tonyhdezpi@gmail.com>
2023-03-08 18:25:40 -0600

    Basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-	return <div className="App">Esto es el todo</div>;
+	return (
+		<div className="App">
+			<h1>To-Do + React</h1>
+			<h2>What's needs to be done?</h2>
+			<input type="text" name="text" className="todo-input" />
+			<button>Add Task</button>
+			<h2>Tasks ramaining: 1</h2>
+			<ul>
+				<li>
+					<input type="checkbox" name="checkedTask" id="1" />
+					<p>Task to complete</p>
+					<button>Edit</button>
+					<button>Delete</button>
+				</li>
+			</ul>
+		</div>
+	);
 }
 
 export default App;
