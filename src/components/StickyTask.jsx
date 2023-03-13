ccdcb47707eb5bb182b3225fa142b9853aab26fb
TonyHdez-Pi <tonyhdezpi@gmail.com>
2023-03-12 22:21:54 -0600

    created StickyTask Component created the styles for StickyTask

---
+++ src/components/StickyTask.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles/stickyTask.css";
+
+export default function StickyTask() {
+	return (
+		<div className="taskContainer">
+			<div className="taskContentContainer">
+				<h2 className="taskTittle">StickyTask :)</h2>
+				<p className="taskContent">
+					TaskContent Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Molestiae architecto porro vero delectus, alias tempore possimus
+					voluptates sequi eos illo natus aperiam voluptate dolor magni
+					inventore, beatae soluta at eius. Lorem ipsum dolor, sit amet
+					consectetur adipisicing elit. Neque alias atque voluptate commodi odit
+					totam eos, distinctio tempore eaque iure nisi dolores eius ad mollitia
+					nihil quaerat. Aperiam, ab doloribus.
+				</p>
+			</div>
+		</div>
+	);
+}
